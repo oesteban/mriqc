5c629ee1219b1951ce4843f56a14345d231302f1
Oscar Esteban <code@oscaresteban.es>
2017-03-04 08:31:58 -0800

    fixing docker cache

--- .circleci/docker-cache.sh
+++ .circleci/docker-cache.sh
@@ -9,4 +9,4 @@ set -e         # Exit immediately if a command exits with a non-zero status.
 set -u         # Treat unset variables as an error when substituting.
 set -x         # Print command traces before executing command.
 
-sudo rsync -a --sparse --exclude 'btrfs' ~/.docker/* /var/lib/docker/
+rsync -a --sparse /home/ubuntu/.var/lib/docker/btrfs ~/.cache/docker/
