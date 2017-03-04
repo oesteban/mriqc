d68589b28b5ecb52819f0358a1897b5fbef4fc90
Oscar Esteban <code@oscaresteban.es>
2017-03-03 23:34:32 -0800

    setting up manual docker cache

---
+++ .circleci/docker-cache.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# Docker cache script
+#
+#
+
+# Setting      # $ help set
+set -e         # Exit immediately if a command exits with a non-zero status.
+set -u         # Treat unset variables as an error when substituting.
+set -x         # Print command traces before executing command.
+
+sudo rsync -a --sparse --exclude 'btrfs' ~/.docker/* /var/lib/docker/
