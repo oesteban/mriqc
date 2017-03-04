cfb6ed6ed7b15254b4b8ce31261ae7cc067a16de
Oscar Esteban <code@oscaresteban.es>
2017-03-04 09:31:07 -0800

    debugging rsyncs

--- .circleci/docker-cache.sh
+++ .circleci/docker-cache.sh
@@ -9,5 +9,4 @@ set -e         # Exit immediately if a command exits with a non-zero status.
 set -u         # Treat unset variables as an error when substituting.
 set -x         # Print command traces before executing command.
 
-rsync -ra --sparse /home/ubuntu/.var/lib/docker/btrfs ~/.cache/docker/
-rsync -ra --exclude 'btrfs' /home/ubuntu/.var/lib/docker/* ~/.cache/docker/
+rsync -ra --sparse /home/ubuntu/.var/lib/docker/* ~/.cache/docker/
