361fe290b3595872cbc1c25266ceae140209d92f
Oscar Esteban <code@oscaresteban.es>
2017-03-04 19:53:28 -0800

    exit early if no docker cache

--- .circleci/docker-restore.sh
+++ .circleci/docker-restore.sh
@@ -9,28 +9,34 @@ set -e         # Exit immediately if a command exits with a non-zero status.
 set -u         # Treat unset variables as an error when substituting.
 set -x         # Print command traces before executing command.
 
-mkdir -p /home/ubuntu/.cache/docker/btrfs/
-mv /home/ubuntu/.cache/docker/btrfs /home/ubuntu/.cache/docker/btrfs-sys
+DOCKER_CACHE_DIR="/home/ubuntu/.cache/docker"
 
-# # Print empty layers
-# find /home/ubuntu/.cache/docker/btrfs-sys/subvolumes -maxdepth 1 \! -empty -print
+if [ -d "${DOCKER_CACHE_DIR}/btrfs" ]; then
+	echo "No docker cache found in ${DOCKER_CACHE_DIR}"
+	exit 0
+fi
+
+
+# Print empty layers
+find ${DOCKER_CACHE_DIR}/btrfs/subvolumes -maxdepth 1 \! -empty -print
 
 # Link layers
-mkdir -p /home/ubuntu/.cache/docker/btrfs/
-btrfs subvolume create /home/ubuntu/.cache/docker/btrfs/subvolumes
-for src in $( ls /home/ubuntu/.cache/docker/btrfs-sys/subvolumes/* ); do
+mv ${DOCKER_CACHE_DIR}/btrfs ${DOCKER_CACHE_DIR}/btrfs-sys
+mkdir -p ${DOCKER_CACHE_DIR}/btrfs
+btrfs subvolume create ${DOCKER_CACHE_DIR}/btrfs/subvolumes
+for src in $( ls ${DOCKER_CACHE_DIR}/btrfs-sys/subvolumes/* ); do
 	layer=$( basename $src )
 
 	if find "$src" -mindepth 1 -print -quit | grep -q .; then
-		btrfs subvolume create /home/ubuntu/.cache/docker/btrfs/subvolumes/$layer
-		cp -r $src/* /home/ubuntu/.cache/docker/btrfs/subvolumes/$layer/
+		btrfs subvolume create ${DOCKER_CACHE_DIR}/btrfs/subvolumes/$layer
+		cp -r $src/* ${DOCKER_CACHE_DIR}/btrfs/subvolumes/$layer/
 	else
 		echo "Layer $layer is empty"
 	fi
 done
 
 # Clean-up broken layers
-# for broken in $( find /home/ubuntu/.cache/docker/btrfs-sys/subvolumes/ -user nobody -type d ); do
-# 	rm /home/ubuntu/.cache/docker/btrfs/subvolumes/$( basename $broken )
+# for broken in $( find ${DOCKER_CACHE_DIR}/btrfs-sys/subvolumes/ -user nobody -type d ); do
+# 	rm ${DOCKER_CACHE_DIR}/btrfs/subvolumes/$( basename $broken )
 # 	echo "Removed pruned broken layer $( basename $broken)"
 # done
