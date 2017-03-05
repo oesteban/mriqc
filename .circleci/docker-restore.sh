4b55711d06bf6b099403dce1a569c99b67de5efe
Oscar Esteban <code@oscaresteban.es>
2017-03-04 19:23:43 -0800

    try using btrfs subvolumes

--- .circleci/docker-restore.sh
+++ .circleci/docker-restore.sh
@@ -13,14 +13,18 @@ mkdir -p /home/ubuntu/.cache/docker/btrfs/
 mv /home/ubuntu/.cache/docker/btrfs /home/ubuntu/.cache/docker/btrfs-sys
 
 # Link layers
-mkdir -p /home/ubuntu/.cache/docker/btrfs/subvolumes
+mkdir -p /home/ubuntu/.cache/docker/btrfs/
+btrfs subvolume create /home/ubuntu/.cache/docker/btrfs/subvolumes
 for src in $( ls /home/ubuntu/.cache/docker/btrfs-sys/subvolumes/* ); do
 	layer=$( basename $src )
-	ln -s $src /home/ubuntu/.cache/docker/btrfs/subvolumes/$layer
+	btrfs subvolume create /home/ubuntu/.cache/docker/btrfs/subvolumes/$layer
+	cp -r $src/* /home/ubuntu/.cache/docker/btrfs/subvolumes/$layer/
 done
 
+find /home/ubuntu/.cache/docker/btrfs/subvolumes/ -user nobody -type d -print
+
 # Clean-up broken layers
-for broken in $( find /home/ubuntu/.cache/docker/btrfs-sys/subvolumes/ -user nobody -type d ); do
-	rm /home/ubuntu/.cache/docker/btrfs/subvolumes/$( basename $broken )
-	echo "Removed pruned broken layer $( basename $broken)"
-done
+# for broken in $( find /home/ubuntu/.cache/docker/btrfs-sys/subvolumes/ -user nobody -type d ); do
+# 	rm /home/ubuntu/.cache/docker/btrfs/subvolumes/$( basename $broken )
+# 	echo "Removed pruned broken layer $( basename $broken)"
+# done
