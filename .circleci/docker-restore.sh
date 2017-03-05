1aa5f508ada05fa889a12fef6c7f5917d16dc40d
Oscar Esteban <code@oscaresteban.es>
2017-03-04 19:39:07 -0800

    check empty layers

--- .circleci/docker-restore.sh
+++ .circleci/docker-restore.sh
@@ -12,16 +12,21 @@ set -x         # Print command traces before executing command.
 mkdir -p /home/ubuntu/.cache/docker/btrfs/
 mv /home/ubuntu/.cache/docker/btrfs /home/ubuntu/.cache/docker/btrfs-sys
 
-# Print empty layers
-find /home/ubuntu/.cache/docker/btrfs-sys/subvolumes -maxdepth 1 \! -empty -print
+# # Print empty layers
+# find /home/ubuntu/.cache/docker/btrfs-sys/subvolumes -maxdepth 1 \! -empty -print
 
 # Link layers
 mkdir -p /home/ubuntu/.cache/docker/btrfs/
 btrfs subvolume create /home/ubuntu/.cache/docker/btrfs/subvolumes
 for src in $( ls /home/ubuntu/.cache/docker/btrfs-sys/subvolumes/* ); do
 	layer=$( basename $src )
-	btrfs subvolume create /home/ubuntu/.cache/docker/btrfs/subvolumes/$layer
-	cp -r $src/* /home/ubuntu/.cache/docker/btrfs/subvolumes/$layer/
+
+	if find "$src" -mindepth 1 -print -quit | grep -q .; then
+		btrfs subvolume create /home/ubuntu/.cache/docker/btrfs/subvolumes/$layer
+		cp -r $src/* /home/ubuntu/.cache/docker/btrfs/subvolumes/$layer/
+	else
+		echo "Layer $layer is empty"
+	fi
 done
 
 # Clean-up broken layers
