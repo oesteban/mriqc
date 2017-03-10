2fcb01bfa7d4db3a9f7a8f5ae9b21b63e31afc27
oesteban <code@oscaresteban.es>
2017-03-10 12:33:51 -0800

    testing btrfs send - fix backup command

--- .circleci/docker-cache.sh
+++ .circleci/docker-cache.sh
@@ -12,12 +12,11 @@ set -x         # Print command traces before executing command.
 DOCKER_ROOT_DIR="/var/lib/docker"
 DOCKER_VOLUMES="${DOCKER_ROOT_DIR}/btrfs/subvolumes"
 
-DOCKER_CACHE_DIR="/home/ubuntu/.cache/docker"
-
 mkdir -p /home/ubuntu/btrfs
 for layerpath in $( find ${DOCKER_VOLUMES} -maxdepth 1 ); do
 	layerid=$( basename $layerpath )
 	echo "Caching layer $layerid"
 	# Cache this layer
-	btrfs subvolume snapshot -r $layerpath /home/ubuntu/btrfs/$layerid | gzip -9 > ${DOCKER_CACHE_DIR}/$layerid.gz
+	btrfs subvolume snapshot -r $layerpath /home/ubuntu/btrfs/$layerid 
+	sudo btrfs send /home/ubuntu/btrfs/$layerid | gzip -9 > ${DOCKER_CACHE_DIR}/$layerid.gz
 done
