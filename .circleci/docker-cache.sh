aca34b4cf7fe1be565e97b6690e67cf79129c77d
oesteban <code@oscaresteban.es>
2017-03-10 12:26:08 -0800

    testing btrfs send

--- .circleci/docker-cache.sh
+++ .circleci/docker-cache.sh
@@ -9,4 +9,15 @@ set -e         # Exit immediately if a command exits with a non-zero status.
 set -u         # Treat unset variables as an error when substituting.
 set -x         # Print command traces before executing command.
 
-rsync -ra --sparse /home/ubuntu/.var/lib/docker/* ~/.cache/docker/
+DOCKER_ROOT_DIR="/var/lib/docker"
+DOCKER_VOLUMES="${DOCKER_ROOT_DIR}/btrfs/subvolumes"
+
+DOCKER_CACHE_DIR="/home/ubuntu/.cache/docker"
+
+mkdir -p /home/ubuntu/btrfs
+for layerpath in $( find ${DOCKER_VOLUMES} -maxdepth 1 ); then
+	layerid=$( basename $layerpath )
+	echo "Caching layer $layerid"
+	# Cache this layer
+	sudo btrfs subvolume snapshot -r $layerpath /home/ubuntu/btrfs/$layerid | gzip -9 > ${DOCKER_CACHE_DIR}/$layerid.gz
+done
